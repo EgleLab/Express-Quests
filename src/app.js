c55050c6f0de68a342a65be2fdbbec7b66297f1a
Egle Laboureur <egle.laboureur@gmail.com>
2024-02-18 19:19:58 +0100

    added_route_Put

--- src/app.js
+++ src/app.js
@@ -12,9 +12,13 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
 app.get("/api/users", userControllers.getUsers);
-app.get("/api/users/:id", userControllers.getUserById);
+app.get("/api/users/:id", userControllers.getUserById); // We declared our route like this; Then in the HANDLER (fonction) we retrieved the id from the URL through req.params.id.
 
 app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/users", userControllers.postUser);
+
+app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/users/:id", userControllers.updateUser); // it's designed to capture the user ID, so you can update a specific user.
+
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -14,7 +14,7 @@ const getMovies = (req, res) => {
 };
 
 const getMovieById = (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);// cette fonction (getMovieById) est our handler qui vient de app.js ou on a declaré la route; on a utilisé req.params property pour retrieve the id
 
   database
     .query("select * from movies where id = ?", [id])
@@ -48,8 +48,31 @@ const postMovie = (req, res) => {
     });
 };
 
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
   postMovie, // don't forget to export your function ;)
+  updateMovie,
 };
